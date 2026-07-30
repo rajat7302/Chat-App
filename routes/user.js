c6695a34eebe7098497625228d7155709b2622f7
rajat7302 <rajatjoshipcmcse@gmail.com>
2026-07-30 19:59:26 +0530

    Fixed the bcrypt thing

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
 const rateLimit = require('express-rate-limit');
 const axios = require('axios');
-
+const bcrypt = require('bcrypt');
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,

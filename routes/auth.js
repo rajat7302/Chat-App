27bfea33fd92093ff5b5dbc9e3936a569114570d
rajat7302 <rajatjoshipcmcse@gmail.com>
2026-07-30 13:52:11 +0530

    Try 2 to fix google sign in

--- routes/auth.js
+++ routes/auth.js
@@ -10,11 +10,11 @@ router.get('/google', passport.authenticate(
 router.get('/google/callback', passport.authenticate('google', {failureRedirect : '/user/signin', session : false}),
 (req, res)=>{
     const token = createTokenForUser(req.user);
-  return res.cookie('token', token, {
-    httpOnly : true,
-    secure : process.env.NODE_ENV === 'production',
-    sameSite : 'lax'
-});
+    return res.cookie('token', token, {
+        httpOnly : true,
+        secure : process.env.NODE_ENV === 'production',
+        sameSite : 'lax'
+    }).redirect('/'); 
 });
 
 module.exports = router;
